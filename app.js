aabe317ed8745a7c4543f9f16b9d902797f032f3
Ayan-farhad <khaanayan81@gmail.com>
2023-11-05 06:55:04 -0800

    continue

---
+++ app.js
@@ -0,0 +1,7 @@
+let todo = document.querySelector(".decor1")
+let ul = document.querySelector(".ul")
+
+function todoDecor(){
+    console.log(todo.value);
+    ul.innerHTML = `<li>monday</li>` ;
+}
